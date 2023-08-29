c33ad199c4c9874af4761384c56a8721a8a2ebd2
Palguni H <23149@omnicell.com>
2023-08-29 11:28:47 +0530

    Final commit of Project

--- shopping-cart/src/app/list-items/list-items.component.ts
+++ shopping-cart/src/app/list-items/list-items.component.ts
@@ -6,7 +6,6 @@ import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarModule, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-list-items',
   templateUrl: './list-items.component.html',
@@ -28,31 +27,24 @@ export class ListItemsComponent implements OnInit {
   notSelected: boolean = false;
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
-<<<<<<< HEAD
   breakpoint: number = 4;
   rowHeight: string = ``;
   showAddToCart = true;
   pincode: number | null = null;
-=======
-
-  pincode : number | null = null;
->>>>>>> palguni
   constructor(private clothingDataService: ClothingDataService, private router: ActivatedRoute,
     private router2: Router, private formBuilder: FormBuilder, private snackBar: MatSnackBar) {
     this.selectedSize = '';
     this.sizeForm = this.formBuilder.group({
       selectedSize: new FormControl('', Validators.required)
     });
-
   }
-
   ngOnInit(): void {
-
+    this.breakpoint = (window.innerWidth <= 1254) ? ((window.innerWidth <= 650) ? 2 : 3) : 4;
+    this.rowHeight = (window.innerWidth <= 1254) ? ((window.innerWidth <= 650) ? `${18}rem` : `${19}rem`) : `${21}rem`;
     this.clothingDataService.getProducts().subscribe(data => {
       this.clothDataList = data;
       this.filteredClothDataList = data;
       this.filteredList = data;
-
       this.router.paramMap.subscribe(params => {
         var parameters = params.get('parameters');
         if (parameters && parameters.length > 0) {
@@ -61,19 +53,16 @@ export class ListItemsComponent implements OnInit {
         }
       });
     });
-
-
   }
-
+  onResize(event: any) {
+    this.breakpoint = (window.innerWidth <= 1254) ? ((window.innerWidth <= 650) ? 2 : 3) : 4;
+    this.rowHeight = (window.innerWidth <= 1254) ? ((window.innerWidth <= 650) ? `${18}rem` : `${19}rem`) : `${21}rem`;
+  }
   showItem(id: number, sidenav: MatSidenav): void {
     this.selectedSize = null;
-<<<<<<< HEAD
     this.selectedProduct = this.clothDataList.find(item => item.id === id) as ClothItem;
     this.showAddToCart = true;
     this.showAddToCart = !this.checkIfAvailable(this.selectedProduct as ClothItem);
-=======
-    this.selectedProduct = this.clothDataList.find(item => item.id === id);
->>>>>>> palguni
     sidenav.open();
   }
   selectSize(size: string) {
@@ -82,34 +71,26 @@ export class ListItemsComponent implements OnInit {
   selectQuantity(event: any) {
     this.selectedQuantity = event.value;
   }
-
   convertStringToObject(parameter: string): { [key: string]: string[] } {
     this.searchresult = '';
     const keyValuePairs = parameter.split('+');
     const result: { [key: string]: string[] } = {};
-
-
     keyValuePairs.forEach(pair => {
       const [key, values] = pair.split('=');
       const parsedValues = values.split(',');
-
       if (!result[key]) {
         result[key] = [];
       }
-
       parsedValues.forEach(value => {
         result[key].push(value);
         this.searchresult = this.searchresult + ' ' + value;
       });
     });
-
     return result;
   }
-
   arrToLower(arr: string[]): string[] {
     return arr.map(item => item.toLowerCase());
   }
-
   addFilter($event: any) {
     let checkedBoxFilter = $event;
     let colorFilter = checkedBoxFilter['color'];
@@ -119,51 +100,40 @@ export class ListItemsComponent implements OnInit {
     let priceFilterArr = this.minMaxPrice(priceFilter);
     let ratingFilterString = checkedBoxFilter['rating'];
     let ratingFilter = ratingFilterString ? ratingFilterString.map(Number) : [];
-
     this.filteredClothDataList = this.filteredList.filter((cloth: any) => {
       let colorMatches = colorFilter ? (colorFilter.length > 0 ? colorFilter.includes(cloth.color) : true) : true;
       let brandMatches = brandFilter ? (brandFilter.length > 0 ? brandFilter.includes(cloth.brand) : true) : true;
       let genderMatches = genderFilter ? (genderFilter.length > 0 ? genderFilter.includes(cloth.gender) : true) : true;
       let ratingMatches = ratingFilter ? (ratingFilter.length > 0 ? ratingFilter.includes(cloth.rating) : true) : true;
       let priceMatches = this.checkPriceFilter(cloth.price, priceFilterArr);
-
       if (colorMatches && brandMatches && genderMatches && ratingMatches && priceMatches) {
         return true;
       }
       return false;
     });
-
   }
-
   checkPriceFilter(price: number, priceFilter: any): boolean {
     if (!priceFilter || Object.keys(priceFilter).length === 0) {
       return true;
     }
-
     for (const [min, max] of priceFilter) {
       if (price >= min && price <= max) {
         return true;
       }
     }
-
     return false;
   }
-
   minMaxPrice(priceFilter: any): any[] {
     const result: any[] = [];
     if (!priceFilter || priceFilter.length == 0) {
       return result;
     }
-
     for (const value of priceFilter) {
       const [min, max] = value.split('-').map(Number);
       result.push([min, max]);
     }
-
     return result;
   }
-
-
   addSearchFilter(clothDataListInput: ClothItem[], searchParameter: any) {
     let checkedBoxFilter = searchParameter;
     let colorFilter = checkedBoxFilter['color'];
@@ -175,7 +145,6 @@ export class ListItemsComponent implements OnInit {
       let brandMatches = brandFilter ? (brandFilter.length > 0 ? brandFilter.includes(cloth.brand.toLowerCase()) : true) : true;
       let genderMatches = genderFilter ? (genderFilter.length > 0 ? genderFilter.includes(cloth.gender.toLowerCase()) : true) : true;
       let categoryMatches = categoryFilter ? (categoryFilter.length > 0 ? categoryFilter.includes(cloth.category.toLowerCase()) : true) : true;
-
       if (colorMatches && brandMatches && genderMatches && categoryMatches) {
         return true;
       }
@@ -183,7 +152,6 @@ export class ListItemsComponent implements OnInit {
     });
     this.filteredClothDataList = this.filteredList;
   }
-
   onSortOptionChange(event: any) {
     const sortBy = event.target.value;
     if (sortBy == 'Ascend') {
@@ -195,11 +163,8 @@ export class ListItemsComponent implements OnInit {
     if (sortBy == 'Rating') {
       this.sortRating();
     }
-
   }
-
   sortAscendPrice() {
-
     const sortedProducts = this.filteredClothDataList.slice().sort((a, b) => a.price - b.price);
     this.filteredClothDataList = sortedProducts;
   }
@@ -207,7 +172,6 @@ export class ListItemsComponent implements OnInit {
     const sortedProducts = this.filteredClothDataList.slice().sort((a, b) => b.price - a.price);
     this.filteredClothDataList = sortedProducts;
   }
-
   sortRating() {
     const sortedProducts = this.filteredClothDataList.slice().sort((a, b) => b.rating - a.rating);
     this.filteredClothDataList = sortedProducts;
@@ -229,7 +193,6 @@ export class ListItemsComponent implements OnInit {
     });
     this.router2.navigate(['/signin']);
     this.addToCart();
-
   }
   addToCart() {
     const currentUser = localStorage.getItem('currentUser');
@@ -251,38 +214,42 @@ export class ListItemsComponent implements OnInit {
     }
     localStorage.setItem(currentUser as string, JSON.stringify(userInfo))
     this.openSnackBar();
-
   }
-
   openSnackBar() {
     this.snackBar.open('Item added to cart', 'Close', {
       horizontalPosition: this.horizontalPosition,
       verticalPosition: this.verticalPosition, duration: 1000,
     });
   }
-  takePincode(event : any){
+  takePincode(event: any) {
     this.pincode = event.target.value;
     event.target.value = null;
   }
-  checkIfAvailable(item : ClothItem) : boolean{
-    if(item.pincode && this.pincode){
-      if(this.pincode <= item.pincode+10 && this.pincode >= item.pincode-10){
+  checkIfAvailable(item: ClothItem): boolean {
+    if (item.pincode && this.pincode) {
+      if (this.pincode <= item.pincode + 10 && this.pincode >= item.pincode - 10) {
         return false;
       }
-      else{
+      else {
         console.log(this.pincode);
         console.log(item)
         return true;
       }
     }
-    else{
+    else {
       return false;
     }
   }
-
 }
 interface cartItem {
   "item": ClothItem,
   "quantity": number,
   "size": string
 }
+
+
+
+
+
+
+
