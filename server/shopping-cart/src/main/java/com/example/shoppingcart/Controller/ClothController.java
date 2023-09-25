18cdbe207bf64e1820a952e70a7f22ef644c00bf
Prashasthi Kanchan <prashasthikanchan2812@gmail.com>
2023-09-25 14:50:55 +0530

    Added get mapping for brands, category and gender

--- server/shopping-cart/src/main/java/com/example/shoppingcart/Controller/ClothController.java
+++ server/shopping-cart/src/main/java/com/example/shoppingcart/Controller/ClothController.java
@@ -1,7 +1,13 @@
 package com.example.shoppingcart.Controller;
 
+import com.example.shoppingcart.Model.Brand;
+import com.example.shoppingcart.Model.Category;
 import com.example.shoppingcart.Model.Cloth;
+import com.example.shoppingcart.Model.Gender;
+import com.example.shoppingcart.Repository.BrandRepository;
+import com.example.shoppingcart.Repository.CategoryRepository;
 import com.example.shoppingcart.Repository.ClothRepository;
+import com.example.shoppingcart.Repository.GenderRepository;
 import java.util.List;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,15 +29,24 @@ public class ClothController {
 	private ClothRepository clothRepository;
 
 	@Autowired
+	private BrandRepository brandRepository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private GenderRepository genderRepository;
+
+	@Autowired
 	private ClothingSearchService clothingSearchService;
 
 	@Autowired
 	private RestHighLevelClient elasticsearchClient;
 
-//  @PostConstruct
-//  public void initializeData() {
-//    clothingSearchService.Indexdata();
-//  }
+	@PostConstruct
+	public void initializeData() {
+		clothingSearchService.Indexdata();
+	}
 
 	private String SearchQueryFinal;
 	List<String> colors = new ArrayList<>();
@@ -52,97 +67,112 @@ public class ClothController {
 		return clothRepository.findAll();
 	}
 
+	@GetMapping("/allBrands")
+	public List<Brand> getAllBrands() {
+		return brandRepository.findAll();
+	}
 
- @GetMapping("/search")
- public List<Object> searchq(@RequestParam("q") String query) {
-   colors = new ArrayList<>();
-   brands = new ArrayList<>();
-   categories = new ArrayList<>();
-   genders = new ArrayList<>();
-   tags = new ArrayList<>();
+	@GetMapping("/allCategory")
+	public List<Category> getAllCategories() {
+		return categoryRepository.findAll();
+	}
 
-   String[] values = query.split(" ");
+	@GetMapping("/allGender")
+	public List<Gender> getAllGenders() {
+		return genderRepository.findAll();
+	}
 
-   for (String value : values) {
-     if (clothingSearchService.getUniqueColors().contains(value)) {
-       colors.add(value);
-     } else if (clothingSearchService.getUniqueBrands().contains(value)) {
-       brands.add(value);
-     } else if (clothingSearchService.getUniqueGender().contains(value)) {
-       genders.add(value);
-     } else if (clothingSearchService.getUniqueCategories().contains(value)) {
-       categories.add(value);
-     } else {
-       tags.add(value);
-     }
-   }
-   String searchQueryAndAggregation = clothingSearchService.buildSearchQueryAndAggregationNoti(colors, brands, genders,
-       categories, tags);
-   SearchQueryFinal = searchQueryAndAggregation;
-   List<Object> finalSearch = clothingSearchService.performAggregation(searchQueryAndAggregation, elasticsearchClient);
-   return finalSearch;
- }
+	@GetMapping("/search")
+	public List<Object> searchq(@RequestParam("q") String query) {
+		colors = new ArrayList<>();
+		brands = new ArrayList<>();
+		categories = new ArrayList<>();
+		genders = new ArrayList<>();
+		tags = new ArrayList<>();
 
- @GetMapping("/updateSearch")
- public List<Object> updateS(@RequestParam("q") String query, @RequestParam("f") String filter) {
-   colorsFilter = new ArrayList<>();
-   brandsFilter = new ArrayList<>();
-   categoriesFilter = new ArrayList<>();
-   gendersFilter = new ArrayList<>();
-   priceFilter = new ArrayList<>();
-   ratingFilter = new ArrayList<>();
+		String[] values = query.split(" ");
 
-   String[] values = query.split(" ");
-   String[] filters = filter.split("&");
+		for (String value : values) {
+			if (clothingSearchService.getUniqueColors().contains(value)) {
+				colors.add(value);
+			} else if (clothingSearchService.getUniqueBrands().contains(value)) {
+				brands.add(value);
+			} else if (clothingSearchService.getUniqueGender().contains(value)) {
+				genders.add(value);
+			} else if (clothingSearchService.getUniqueCategories().contains(value)) {
+				categories.add(value);
+			} else {
+				tags.add(value);
+			}
+		}
+		String searchQueryAndAggregation = clothingSearchService.buildSearchQueryAndAggregationNoti(colors, brands,
+				genders, categories, tags);
+		SearchQueryFinal = searchQueryAndAggregation;
+		List<Object> finalSearch = clothingSearchService.performAggregation(searchQueryAndAggregation,
+				elasticsearchClient);
+		return finalSearch;
+	}
 
-   for (String value : values) {
-     if (clothingSearchService.getUniqueColors().contains(value)) {
-       colors.add(value);
-     } else if (clothingSearchService.getUniqueBrands().contains(value)) {
-       brands.add(value);
-     } else if (clothingSearchService.getUniqueGender().contains(value)) {
-       genders.add(value);
-     } else if (clothingSearchService.getUniqueCategories().contains(value)) {
-       categories.add(value);
-     } else {
-       tags.add(value);
-     }
-   }
+	@GetMapping("/updateSearch")
+	public List<Object> updateS(@RequestParam("q") String query, @RequestParam("f") String filter) {
+		colorsFilter = new ArrayList<>();
+		brandsFilter = new ArrayList<>();
+		categoriesFilter = new ArrayList<>();
+		gendersFilter = new ArrayList<>();
+		priceFilter = new ArrayList<>();
+		ratingFilter = new ArrayList<>();
 
-   for (String f : filters) {
-     String[] keyValue = f.split("=");
-     String key = keyValue[0];
-     String value = keyValue[1];
-     if ("Color".equals(key)) {
-       colorsFilter.add(value);
-     } else if ("Brand".equals(key)) {
-       brandsFilter.add(value);
-     } else if ("Gender".equals(key)) {
-       gendersFilter.add(value);
-     } else if ("Category".equals(key)) {
-       categoriesFilter.add(value);
-     } else if ("Price".equals(key)) {
-       String[] priceBounds = value.split("-");
-       if (priceBounds.length == 2) {
-         String minPrice = priceBounds[0];
-         String maxPrice = priceBounds[1];
-         priceFilter.add(minPrice + "-" + maxPrice);
-       }
-     } else if ("Rating".equals(key)) {
-       String[] ratingBounds = value.split("-");
-       if (ratingBounds.length == 2) {
-         String minRating = ratingBounds[0];
-         String maxRating = ratingBounds[1];
-         ratingFilter.add(minRating + "-" + maxRating);
-       }
-     }
-   }
-   String addPostFilterQuery = clothingSearchService.addPostFilter(SearchQueryFinal, colorsFilter, brandsFilter,
-       priceFilter, gendersFilter, categoriesFilter, ratingFilter, elasticsearchClient);
-   List<Object> finalSearch = clothingSearchService.performAggregation(addPostFilterQuery, elasticsearchClient);
+		String[] values = query.split(" ");
+		String[] filters = filter.split("&");
 
-   return finalSearch;
+		for (String value : values) {
+			if (clothingSearchService.getUniqueColors().contains(value)) {
+				colors.add(value);
+			} else if (clothingSearchService.getUniqueBrands().contains(value)) {
+				brands.add(value);
+			} else if (clothingSearchService.getUniqueGender().contains(value)) {
+				genders.add(value);
+			} else if (clothingSearchService.getUniqueCategories().contains(value)) {
+				categories.add(value);
+			} else {
+				tags.add(value);
+			}
+		}
 
- }
+		for (String f : filters) {
+			String[] keyValue = f.split("=");
+			String key = keyValue[0];
+			String value = keyValue[1];
+			if ("Color".equals(key)) {
+				colorsFilter.add(value);
+			} else if ("Brand".equals(key)) {
+				brandsFilter.add(value);
+			} else if ("Gender".equals(key)) {
+				gendersFilter.add(value);
+			} else if ("Category".equals(key)) {
+				categoriesFilter.add(value);
+			} else if ("Price".equals(key)) {
+				String[] priceBounds = value.split("-");
+				if (priceBounds.length == 2) {
+					String minPrice = priceBounds[0];
+					String maxPrice = priceBounds[1];
+					priceFilter.add(minPrice + "-" + maxPrice);
+				}
+			} else if ("Rating".equals(key)) {
+				String[] ratingBounds = value.split("-");
+				if (ratingBounds.length == 2) {
+					String minRating = ratingBounds[0];
+					String maxRating = ratingBounds[1];
+					ratingFilter.add(minRating + "-" + maxRating);
+				}
+			}
+		}
+		String addPostFilterQuery = clothingSearchService.addPostFilter(SearchQueryFinal, colorsFilter, brandsFilter,
+				priceFilter, gendersFilter, categoriesFilter, ratingFilter, elasticsearchClient);
+		List<Object> finalSearch = clothingSearchService.performAggregation(addPostFilterQuery, elasticsearchClient);
+
+		return finalSearch;
+
+	}
 
 }
